4ca1d09e3b91b7d4b2838e88f98bf08a352bc91b
Paul Williams <abercromby@gmail.com>
2017-11-15 12:24:13 +0000

    Added database store

---
+++ UserAuthenticator/UserAuthentication.Store.Database/AuthContext.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAuthentication.Store.Database
+{
+    using System.Data.Entity;
+
+    public sealed class AuthContext : DbContext
+    {
+        public IDbSet<AuthenticationDetailsEntity> AuthenticationDetails { get; set; }
+    }
+}
--- UserAuthenticator/UserAuthentication.Store.Database/AuthenticationDetailsStoreDatabase.cs
+++ UserAuthenticator/UserAuthentication.Store.Database/AuthenticationDetailsStoreDatabase.cs
@@ -1,16 +1,34 @@
 ﻿namespace UserAuthentication.Store.Database
 {
+    using System;
+    using System.Linq;
     using UserAuthentication.Messages;
 
     public sealed class AuthenticationDetailsStoreDatabase : IAuthenticationDetailsStore
     {
+        private readonly AuthContext context = new AuthContext();
+
         public AuthenticationDetails GetByEmail(string email)
         {
-            return null;
+            return this.context.Set<AuthenticationDetailsEntity>()
+                .Select(a => new AuthenticationDetails
+                {
+                    Email = a.Email,
+                    Password = a.Password
+                })
+                .FirstOrDefault(a => a.Email == email);
         }
 
         public void Insert(AuthenticationDetails details)
         {
+            this.context.Set<AuthenticationDetailsEntity>().Add(
+                new AuthenticationDetailsEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Email = details.Email,
+                    Password = details.Password
+                });
+            this.context.SaveChanges();
         }
     }
 }
