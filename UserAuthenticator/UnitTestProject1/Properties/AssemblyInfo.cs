2be718934cb6697eb9d7e98482b70b8df820cf8f
Paul Williams <paul.williams@menziesdistribution.com>
2017-11-16 00:40:16 +0000

    Tidying up

--- UserAuthenticator/UnitTestProject1/Properties/AssemblyInfo.cs
+++ UserAuthenticator/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("UserAuthentication.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("UserAuthentication.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
